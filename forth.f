62ed7222dab66a0bee5dde0d96ad108234304bf1
s3e8 <sphiax.s3e8@gmail.com>
2026-03-08 04:48:15 +0000

    did some more stuff... still works

--- forth.f
+++ forth.f
@@ -169,38 +169,35 @@ datahere0 !
 : pick 1+ cellsize * dsp@ + @ ;
 
 
+: ? @ . ;
+: ?iscall   >cfa @ ' call = ;
+
+: alias todo ;
 
 
 
 
 
 
-
-( sanakirjat )
+( dictionaries )
 variable current-vocab
 variable latest-defined-vocab
 
 0 current-vocab !
 0 latest-defined-vocab !
 
-: vocab-name ( vocabentry -- name ) cell+ @ ;
-: vocab-next ( vocabentry -- nextvocabentry/0 ) 2 cells + @ ;
-: vocab-latest ( vocab-entry -- latest ) @ ;
-: set-vocab-name ( name vocabentry -- ) cell+ ! ;
-: set-vocab-next ( nextentry vocabentry -- ) 2 cells + ! ;
-: set-vocab-latest ( latest vocabentry -- ) ! ;
-: vocab-useslist ( vocabentry -- useslist ) 3 cells + ;
+: vocab-name        ( vocabentry -- name )              cell+ @     ;
+: vocab-next        ( vocabentry -- nextvocabentry/0 )  2 cells + @ ;
+: vocab-latest      ( vocab-entry -- latest )           @           ;
+: set-vocab-name    ( name vocabentry -- )              cell+ !     ;
+: set-vocab-next    ( nextentry vocabentry -- )         2 cells + ! ;
+: set-vocab-latest  ( latest vocabentry -- )            !           ;
+: vocab-useslist    ( vocabentry -- useslist )          3 cells +   ;
 
 
 
 
-: ? @ . ;
-: ?iscall   >cfa @ ' call = ;
 
-
-
-
-: alias todo ;
 
 
 
